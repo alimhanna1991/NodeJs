1c56206b541aa0164a182c00a6b749ffadbbb313
alimhanna1991 <33596355+alimhanna1991@users.noreply.github.com>
2022-01-30 11:27:29 +0200

    Http Module

--- gettingStarted/myfirst.js
+++ gettingStarted/myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
-
+var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write('the time now is' + dt.myDateTime());
+  res.end();
 }).listen(8080);
